40656c9747d76f4cbc660031b57524536250d6c4
socalpete007 <socalpete@mac.com>
2026-04-25 20:30:16 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
+/**
+ * This is the entry point for your React application.
+ * It takes your App component and injects it into the 
+ * HTML element with the ID 'root' found in index.html.
+ */
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
